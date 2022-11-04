5afa6e724c4d023577c70f2df1316ef979dab0de
volle1212 <94704890+volle1212@users.noreply.github.com>
2022-11-04 23:09:14 +0100

    remake
    new design and some added features

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from random import randint
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    r = randint(0,255)
+    g = randint(0,255)
+    b = randint(0,255)
+    return render_template("index.html",r=r, g=g, b=b, int=int)
+
+
+@app.route('/answer', methods=['GET', 'POST'])
+def answer():
+    r = request.form["r"]
+    g = request.form["g"]
+    b = request.form["b"]
+
+    r_answer = request.form["r_answer"]
+    g_answer = request.form["g_answer"]
+    b_answer = request.form["b_answer"]
+    
+    points = (abs(int(r_answer)-int(r)) + abs(int(g_answer)-int(g)) + abs(int(b_answer)-int(b)))
+
+    return render_template("answer.html", points=points, r=r, g=g, b=b, int=int, r_answer=r_answer, g_answer=g_answer, b_answer=b_answer)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
